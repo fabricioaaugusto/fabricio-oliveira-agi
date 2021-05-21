b547cb0e6b70edc559dd84638556b040b2ee7523
Fabricio Oliveira <fabricioaaugusto@gmail.com>
2021-05-21 00:40:33 -0300

    first test done

--- cypress/integration/specs/firstTest.spec.js
+++ cypress/integration/specs/firstTest.spec.js
@@ -1,5 +1,21 @@
-describe('My First Test', function() {
-    it('Does not do much!', function() {
-      expect(true).to.equal(true)
-    })
-})
+describe('My First Test', () => {
+    it('visit Site', () => {
+      cy.visit('https://easyevents-v2.herokuapp.com/');
+    });
+
+    it('verify header site', () => {
+      cy
+        .get('#mainNav')
+        .find('.nav-item')
+        .eq(0)
+        .should('have.text', '\n          Sobre\n        ')
+        .get('#mainNav')
+        .find('.nav-item')
+        .eq(1)
+        .should('have.text', '\n          Serviço\n        ')
+        .get('#mainNav')
+        .find('.nav-item')
+        .eq(2)
+        .should('have.text', '\n          Contato\n        ');
+    });
+})  
