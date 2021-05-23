d1644c29834fd1b7c3918e171ee8faee104ab329
Fabricio Oliveira <fabricioaaugusto@gmail.com>
2021-05-23 12:04:50 -0300

    update branch

--- cypress/integration/specs/firstTest.spec.js
+++ cypress/integration/specs/firstTest.spec.js
@@ -1,11 +1,13 @@
 import easyevents from '../pages/easySite';
 
 describe('My First Test', () => {
-    it('visit Site', () => {
-      easyevents.getVisitSite();
-    });
+  it('visit Site', () => {
+    easyevents.getVisitSite();
+  });
 
-    it('verify header site', () => {
-      easyevents.assertHeader('\n          Sobre\n        ', '\n          Serviço\n        ', '\n          Contato\n        ');
-    });
+  it('verify header site', () => {
+    easyevents.assertHeader('\n          Sobre\n        ', '\n          Serviço\n        ', '\n          Contato\n        ');
+  });
+
+    
 })  
