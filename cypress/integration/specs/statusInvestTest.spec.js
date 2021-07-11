0ea6e09630ec9f2bd4d9cfb955b1b7ab97173ec5
Fabricio <fabricioaaugusto@gmail.com>
2021-07-11 15:35:07 -0300

    update branch

--- cypress/integration/specs/statusInvestTest.spec.js
+++ cypress/integration/specs/statusInvestTest.spec.js
@@ -1,7 +1,7 @@
 import statusInvestSite from '../pages/statusInvestSite'; 
 
 describe('Tests e2e Site StatusInvest', () => {
-  it('visit Site', () => {
+  it('visit site status invest', () => {
     statusInvestSite.getVisitSite();
   });
 
