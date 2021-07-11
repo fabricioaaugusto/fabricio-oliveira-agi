cb4960d589e3a4d2473ac15e37585bc5f3718535
Fabricio <fabricioaaugusto@gmail.com>
2021-07-11 15:41:30 -0300

    update

--- cypress/integration/specs/statusInvestTest.spec.js
+++ cypress/integration/specs/statusInvestTest.spec.js
@@ -3,7 +3,6 @@ import statusInvestSite from '../pages/statusInvestSite';
 describe('Tests e2e Site StatusInvest', () => {
   it('visit site status invest', () => {
     statusInvestSite.getVisitSite();
-    
   });
 
   it('visit close modal if open', () => {
